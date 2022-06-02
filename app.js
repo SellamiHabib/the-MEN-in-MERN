5deeedea2aa205f723252e76f96b2ac20b3d2de0
Habib Sellami <habib.sellami@outlook.com>
2022-06-02 14:04:21 +0100

    add static pages routing

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const http = require('http');
 const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
 app.use((req, res, next) => {
-    console.log("In the first middleware");
-    next();
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
-
-app.use((req,res,next) => { 
-    console.log("Joined the second middleware!");
-    res.send("<h1>")
- })
-
-const server = http.createServer(app)
-server.listen(port);
+app.listen(port);
